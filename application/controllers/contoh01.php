21c725763a6b91421b1b75ca26a0b6a68ba6095e
syarlanasywa <syarlanasywa@gmail.com>
2023-09-25 15:29:30 +0700

    Tugas Pertemuan 1

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh01 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Syarla Nabila Nasywa Saya tinggal di daerah cibitung olahraga yang saya sukai adalah basket";
+
+    }
+}
+?>
