8829cef5da6067eb26307470188213fbf65eb2d7
syarlanasywa <syarlanasywa@gmail.com>
2023-09-25 16:59:21 +0700

    Latihan 1

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,16 @@
+ <?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Biodata Diri</h1>";
+        echo "Nama : Syarla Nabila Nasywa
+        <p>Nim : 19220836
+        <p>No hp : 085772259215
+        <p>Kelas : 19.3A.24
+        <p>Hobi : Basket
+        <p>Alamat : Cibitung
+        <p>Umur : 19 tahun</p> ";
+    }
+}
+?>
