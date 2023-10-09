ee941b1cd51d1bbc47a5eb63b5daa8b3e71689b9
syarlanasywa <syarlanasywa@gmail.com>
2023-10-09 10:48:40 +0700

    Tugas Pertemuan 2

--- application/controllers/contoh01.php
+++ application/controllers/contoh01.php
@@ -4,7 +4,8 @@ class Contoh01 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Syarla Nabila Nasywa Saya tinggal di daerah cibitung olahraga yang saya sukai adalah basket";
+        echo "Nama saya Syarla Nabila Nasywa
+        Saya tinggal di daerah cibitung olahraga yang saya sukai adalah basket";
 
     }
 }
